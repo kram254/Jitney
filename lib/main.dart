11248de80ac9b0fdd0d78ac126197cf2aec0d859
ndaliro <kram254@github.com>
2020-12-24 00:15:25 +0300

    UserLocation and Permissions

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:jitney/screens/home.dart';
 import 'package:provider/provider.dart';
 import 'package:jitney/providers/app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:jitney/helpers/constants.dart';
 
 
-void main (){
+void main () async{
+WidgetsFlutterBinding.ensureInitialized();
+SharedPreferences prefs = await SharedPreferences.getInstance();
+LocationPermission permission = await Geolocator.checkPermission();
+bool _isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+bool _hasLocationPermission = prefs.getBool(HAS_PERMISSION) ?? false;
+//if has location_permission is null by default its going to be set to false
+ _isLocationEnabled =prefs.getBool(LOCATION_ENABLED) ?? false;
+
+
+
+if(!_isLocationEnabled){
+
+  await Geolocator.openLocationSettings();
+
+}else{
+  if(!_hasLocationPermission){
+  if(permission != LocationPermission.always){
+  LocationPermission locationPermission = await Geolocator.requestPermission();
+
+  if(Geolocator.requestPermission() == LocationPermission.always){
+      await prefs .setBool(HAS_PERMISSION, true);
+  }else {
+    // TODO:: do something
+  }
+}
+}
+}
+
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider.value(value: AppProvider.initialize())
   ],
